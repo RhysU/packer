0826cacdbe43fe69a6871ded2b006bec7291dc99
Rhys Ulerich <Rhys.Ulerich@gmail.com>
2021-08-31 23:21:01 -0400

    Add MPL2 headers

--- src/com/example/onnx/java/TestPacker.java
+++ src/com/example/onnx/java/TestPacker.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2021 Rhys Ulerich <rhys.ulerich@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.example.onnx.java;
 
 import org.junit.jupiter.api.Test;
