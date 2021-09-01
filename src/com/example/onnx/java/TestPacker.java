ac1fcad5786f6dab867db5305441b321ea57f524
Rhys Ulerich <Rhys.Ulerich@gmail.com>
2021-08-31 23:03:05 -0400

    Complete testOneColumn

--- src/com/example/onnx/java/TestPacker.java
+++ src/com/example/onnx/java/TestPacker.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.DoubleBuffer;
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 final class TestPacker {
 
@@ -11,31 +18,67 @@ final class TestPacker {
     private enum Column { A, B, C, D }
 
     @Test
-    void testSingle() {
+    void testOneColumn() {
         // Allocate storage for one single column
         final Packer<Column>.DoubleStorage s =
-                new Packer<>(Column.A).new DoubleStorage(4);
-        Assertions.assertEquals(4, s.capacity());
+                new Packer<>(Column.B).new DoubleStorage(4);
+        assertEquals(4, s.capacity());
 
         // Fill the storage
-        Assertions.assertTrue(s.hasRemaining());
-        s.put(Column.A, 555);
-        Assertions.assertTrue(s.hasRemaining());
-        s.put(Column.A, 666);
-        Assertions.assertTrue(s.hasRemaining());
-        s.put(Column.A, 777);
-        Assertions.assertTrue(s.hasRemaining());
-        s.put(Column.A, 888);
-        Assertions.assertFalse(s.hasRemaining());
+        assertTrue(s.hasRemaining());
+        s.put(Column.B, 555);
+        assertTrue(s.hasRemaining());
+        s.put(Column.A, 12);  // Ignored
+        s.put(Column.B, 666);
+        s.put(Column.C, 34);  // Ignored
+        s.put(Column.D, 56);  // Ignored
+        assertTrue(s.hasRemaining());
+        // A and D ignored while C not supplied
+        s.put(new EnumMap<>(Map.of(Column.A, 1, Column.B, 777, Column.D, 2)));
+        assertTrue(s.hasRemaining());
+        s.put(Column.B, 888);
+        assertFalse(s.hasRemaining());
 
         // Confirm all rows as expected
-        final DoubleBuffer b = s.discard(0, 0);
-        Assertions.assertEquals(0, b.position());
-        Assertions.assertEquals(4, b.limit());
-        Assertions.assertEquals(4, b.capacity());
-        Assertions.assertEquals(555.0, b.get());
-        Assertions.assertEquals(666.0, b.get());
-        Assertions.assertEquals(777.0, b.get());
-        Assertions.assertEquals(888.0, b.get());
+        final DoubleBuffer b1 = s.discard(0, 0);
+        assertEquals(0, b1.position());
+        assertEquals(4, b1.limit());
+        assertEquals(4, b1.capacity());
+        assertTrue(b1.hasRemaining());
+        assertEquals(555.0, b1.get());
+        assertEquals(666.0, b1.get());
+        assertEquals(777.0, b1.get());
+        assertEquals(888.0, b1.get());
+        assertFalse(b1.hasRemaining());
+
+        // Discard the last row and reconfirm
+        final DoubleBuffer b2 = s.discard(0, 1);
+        assertEquals(0, b2.position());
+        assertEquals(3, b2.limit());
+        assertEquals(4, b2.capacity());
+        assertTrue(b2.hasRemaining());
+        assertEquals(555.0, b2.get());
+        assertEquals(666.0, b2.get());
+        assertEquals(777.0, b2.get());
+        assertFalse(b2.hasRemaining());
+        assertSame(b1, b2, "discard(...) reuses");
+
+        // Discard the first two rows and reconfirm
+        final DoubleBuffer b3 = s.discard(2, 0);
+        assertEquals(0, b3.position());
+        assertEquals(1, b3.limit());
+        assertEquals(4, b3.capacity());
+        assertTrue(b3.hasRemaining());
+        assertEquals(777.0, b3.get());
+        assertFalse(b3.hasRemaining());
+        assertSame(b1, b3, "discard(...) reuses");
+
+        // Discard more than remaining rows and reconfirm
+        final DoubleBuffer b4 = s.discard(9, 9);
+        assertEquals(0, b4.position());
+        assertEquals(0, b4.limit());
+        assertEquals(4, b4.capacity());
+        assertFalse(b4.hasRemaining());
+        assertSame(b1, b4, "discard(...) reuses");
     }
 }
